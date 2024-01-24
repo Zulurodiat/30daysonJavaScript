26376dde1c4aa5b7b33b4b0bdadf58b455c9279d
zulurodiat <zulurodiat@gmail.com>
2024-01-24 12:17:02 +0100

    first commit

---
+++ day01/datatypes.js
@@ -0,0 +1,18 @@
+let string= "hi";
+let num= 20;
+let bol= true;
+let undefinedVariable;
+console.log(typeof string);
+console.log(typeof bol);
+console.log(typeof num);
+console.log(typeof undefinedVariable);
+
+
+
+
+
+
+
+
+
+
---
+++ day01/variables.js
@@ -0,0 +1,42 @@
+//comments can make code readable
+    /*welcome to 30days0nJavascript
+    /*comments can make code readable, easy to reuse and informative
+
+    let secondName= "";
+    let newAge="";
+
+let string= "Yusuf";
+let num= "20";
+let bol= "true";
+let backTick= "ope";
+console.log(string);
+console.log(num);
+console.log(bol);
+console.log(backTick);
+
+/*let firstname= "Rodiat";
+console.log(firstname);
+ let lastname="Zulu";
+ console.log(lastname);
+ let maritalstatus= "Single";
+ console.log(maritalstatus);
+let country="Nigeria";
+console.log(country);
+let age= "21";
+console.log(age);*/
+
+let firstname= "Rodiat";
+console.log(firstname);
+let lastname="Zulu";
+console.log(lastname);
+let maritalstatus= "Single";
+console.log(maritalstatus);
+let country="Nigeria";
+console.log(country);
+let age= "21";
+console.log(age);
+
+let myAge= "i am 25 years old";
+console.log(myAge)
+let yourAge= "you are 30 years old";
+console.log(yourAge)
